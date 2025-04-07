7dafaeb2b10f644e033b37d78232bda19db75ea1
Mohit Kumar <mohit.janjadiya@gmail.com>
2025-04-07 06:08:03 +0530

    updated codes till lecture 11

--- index.js
+++ index.js
@@ -26,3 +26,28 @@ function x(){
     y();
 }
 x();
+
+// closurewithsetTimeout;
+
+function y(){
+    // for(var i = 1; i<=5; i++){
+    for(var i = 1; i<=5; i++){
+        setTimeout(function(){
+            console.log(i); //setTimeout+closure
+        }, 1000);//by this time i will be 6 so the output is 6
+        //to rectify it we use let insted of var as let provide fresh copy of the variable in each iteration of the loop 
+    }
+}
+y(); // 1
+
+//this can be solved by using closure as well instead of using let
+function y(){
+    for(var i = 1; i<=5; i++){
+        function closure(i){
+            setTimeout(function(){
+                console.log(i); //setTimeout+closure
+            }, 1000);//by this time i will be 6 so the output is 6
+        }
+        closure(i);
+    }
+}
