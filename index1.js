67cc166e12235c9a53101ad5f4661ab68c92637b
Mohit Kumar <mohit.janjadiya@gmail.com>
2025-04-08 06:30:44 +0530

    updated codes

--- index1.js
+++ index1.js
@@ -1,12 +1,12 @@
 //Function Statement
-function a(){
-    console.log("a called");
-}
+// function a(){
+//     console.log("a called");
+// }
 
 //Function Expression
-var b = function(){
-    console.log("b called");
-}
+// var b = function(){
+//     console.log("b called");
+// }
 
 //Anonymous Function
 // function(){
@@ -14,17 +14,40 @@ var b = function(){
 // }
 
 //named function expression
-function d(){
-    console.log("d called");
-}
-d(); //it is not created as a local variable, so it cannot be called outside of the function
+// function d(){
+//     console.log("d called");
+// }
+// d(); //it is not created as a local variable, so it cannot be called outside of the function
 
 //first class function
-function c(fn){
-    fn();
-}//function are first class citizens in JS, they can be passed as arguments to other functions, returned from other functions, and assigned to variables.
+// function c(fn){
+//     fn();
+// }//function are first class citizens in JS, they can be passed as arguments to other functions, returned from other functions, and assigned to variables.
 
 //arrow function
-const e = () => {
-    console.log("e called");
+// const e = () => {
+//     console.log("e called");
+// }
+
+//callback function
+// setTineout(function(){
+//     console.log("callback function called");
+// }, 1000);
+// function x(y){
+//     console.log("x called");
+// }
+// x(function y(){
+//     console.log("y called");
+// });
+
+function attachEvent(){
+    let count = 0;
+    document.getElementById("ClickMe")
+    .addEventListener("click",function(){
+        console.log("Button clicked",++count);
+    });
 }
+attachEvent();
+
+//garbage collection and remove event listener
+
