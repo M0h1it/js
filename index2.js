3f1d14c0aaca42a2d7f883a6eadd1d8fbc6f3996
Mohit Kumar <mohit.janjadiya@gmail.com>
2025-04-18 06:29:45 +0530

    namaste javascript season 1 complete

--- index2.js
+++ index2.js
@@ -20,3 +20,26 @@ const calculate = function(radius, logic) {
 
 const areas = calculate(radius, area);
 const circumferences = calculate(radius, circumference);
+
+
+//map function
+arr = [1,2,3,4,5,6,7,8,9,10];
+const double = function(num) {
+    return num * 2;
+}
+const doubleArr = arr.map(double);
+console.log(doubleArr); // [2,4,6,8,10,12,14,16,18,20]
+
+//filter function
+const isEven = function(num) {
+    return num % 2 === 0;
+}
+const evenArr = arr.filter(isEven);
+console.log(evenArr); // [2,4,6,8,10]
+
+//reduce function
+const sum = function(acc, num) {
+    return acc + num;
+}
+const total = arr.reduce(sum, 0);
+console.log(total); // 55
