5ff16d9b36f7ef9437e19a88ebe221ed68e26f30
Mohit Kumar <mohit.janjadiya@gmail.com>
2025-04-20 18:21:45 +0530

    lecture 2 completed

--- indexS21.js
+++ indexS21.js
@@ -5,7 +5,7 @@
 
 // console.log("Season 2");
 
-const cart = ["shoes"," pants", "kurta"];
+// const cart = ["shoes"," pants", "kurta"];
 
 api.createorder(cart,function(){
 
@@ -16,5 +16,30 @@ api.createorder(cart,function(){
         })
 
     })
-    
+
 })
+
+
+//promimses
+// const cart = ["shoes"," pants", "kurta"];
+
+// const promise = createorder(cart);
+
+// promise.then(function(){
+//     return proceedTopayment();
+// }).then(function(){
+//     return showorderSummary();
+// }).then(function(){
+//     console.log("order placed successfully")
+// }).catch(function(){
+//     console.log("some error occured")
+// })
+
+const GITHUM_API = "https://api.github.com/users";
+
+const user = fetch(GITHUM_API);
+
+console.log(user);
+
+
+
